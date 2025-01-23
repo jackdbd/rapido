17cde5530efeca41e8efec9e7f3111dadd173e77
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-01-23 16:24:30 +0100

    fix(oauth2-tokens): export schemas

--- packages/oauth2-tokens/src/index.ts
+++ packages/oauth2-tokens/src/index.ts
@@ -14,7 +14,31 @@ export { safeDecode } from "./decode-jwt.js";
 export { sign, type SignConfig } from "./sign-jwt.js";
 export { verify, type VerifyConfig } from "./verify-jwt.js";
 
-export { tokensPlusInfo } from "./tokens-plus-info.js";
+export {
+  tokensPlusInfo,
+  return_value_schema as tokens_plus_info,
+} from "./tokens-plus-info.js";
 export type { ReturnValue as TokensPlusInfo } from "./tokens-plus-info.js";
 
-export * from "./schemas/index.js";
+export {
+  alg,
+  exp,
+  expiration,
+  iat,
+  iss,
+  jti,
+  jwk_private,
+  jwk_public,
+  jwks_private,
+  jwks_public,
+  jwks_url,
+  jwt,
+  kid,
+} from "./schemas/index.js";
+export type {
+  JWKPrivate,
+  JWKPublic,
+  JWKSPrivate,
+  JWKSPublic,
+  JWKSPublicURL,
+} from "./schemas/index.js";
