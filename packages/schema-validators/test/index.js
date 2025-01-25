0046d226494e36bbfbcbe72639c68cd580233b1c
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-01-25 17:05:47 +0100

    add some more tests

--- packages/schema-validators/test/index.js
+++ packages/schema-validators/test/index.js
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import { describe, it } from "node:test";
 import { Type } from "@sinclair/typebox";
 import { defAjv } from "../../stdlib/lib/test-utils.js";
-import { conformResult } from "../lib/index.js";
+import { conformResult, throwWhenNotConform } from "../lib/index.js";
 
 const ajv = defAjv();
 
@@ -23,5 +23,22 @@ describe("conformResult", () => {
     assert.ok(!error);
     assert.ok(value);
     assert.equal(value.validated, data);
+  });
+});
+
+describe("throwWhenNotConform", () => {
+  it("throws when data does not conform", () => {
+    const schema = Type.String({ minLength: 1 });
+
+    assert.throws(() => {
+      throwWhenNotConform({ ajv, schema, data: "" });
+    });
+  });
+
+  it("returns undefined when data conforms", () => {
+    const schema = Type.String({ minLength: 1 });
+    const value = throwWhenNotConform({ ajv, schema, data: "hello" });
+
+    assert.equal(value, undefined);
   });
 });
