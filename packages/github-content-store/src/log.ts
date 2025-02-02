53965c836c3f46f7732baf5d4e1041433fee4054
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-02-02 23:04:28 +0100

    fix(github-content-store): add tests

--- packages/github-content-store/src/log.ts
+++ packages/github-content-store/src/log.ts
@@ -2,3 +2,8 @@ export interface Log {
   debug: (...args: any) => void;
   error: (...args: any) => void;
 }
+
+export const defaultLog: Log = {
+  debug: () => {},
+  error: () => {},
+};
--- packages/github-content-store/test/index.js
+++
@@ -1,5 +0,0 @@
-import { describe, it } from "node:test";
-
-describe("GitHub content store", () => {
-  it.todo("write some tests");
-});
