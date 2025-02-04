078b9fe63f4309ce848e9a729a1a1246db7fbd77
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-02-04 17:15:44 +0100

    simplify ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,46 @@
 import eslint from '@eslint/js'
 import repoPrettierConfig from '@repo/prettier-config'
 import prettierConfig from 'eslint-config-prettier'
-import onlyWarnPlugin from 'eslint-plugin-only-warn'
-import prettierPlugin from 'eslint-plugin-prettier'
-import turboPlugin from 'eslint-plugin-turbo'
+import onlyWarn from 'eslint-plugin-only-warn'
+import prettier from 'eslint-plugin-prettier'
+import turbo from 'eslint-plugin-turbo'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-// https://typescript-eslint.io/users/what-about-formatting/#suggested-usage---prettier
-// https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/README.md
-// https://github.com/prettier/eslint-config-prettier
-// https://github.com/bfanger/eslint-plugin-only-warn
-
 const config = [
+  // https://github.com/eslint-recommended/eslint-config
   eslint.configs.recommended,
+  // https://typescript-eslint.io/getting-started
   ...tseslint.configs.recommended,
-  // configuration for eslint-plugin-turbo
-  {
-    plugins: {
-      turbo: turboPlugin
-    },
-    rules: {
-      'turbo/no-undeclared-env-vars': 'warn'
-    }
-  },
-  // configuration for eslint-plugin-prettier
-  {
-    plugins: {
-      prettier: prettierPlugin
-    },
-    rules: {
-      'prettier/prettier': [
-        'error',
-        { ...prettierConfig, ...repoPrettierConfig }
-      ] // enforce Prettier rules
-    }
-  },
-  // configuration for eslint-plugin-only-warn
-  {
-    plugins: {
-      onlyWarn: onlyWarnPlugin
-    }
-  },
   {
     ignores: ['dist/**'],
     languageOptions: {
       globals: {
         ...globals.node
       }
+    },
+    plugins: {
+      // https://github.com/bfanger/eslint-plugin-only-warn
+      onlyWarn,
+      // https://github.com/prettier/eslint-plugin-prettier
+      prettier,
+      // https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/README.md
+      turbo
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      'prettier/prettier': [
+        'error',
+        { ...prettierConfig, ...repoPrettierConfig }
+      ],
+      'turbo/no-undeclared-env-vars': 'warn'
     }
   }
 ]
