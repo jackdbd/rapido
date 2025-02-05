0fd14e4e05b8c39622c4a7d0e87daf3ece992c82
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-02-05 09:47:52 +0100

    vendorize error-handlers in all indieauth/micropub plugins

--- packages/fastify-token-endpoint/src/index.ts
+++ packages/fastify-token-endpoint/src/index.ts
@@ -1,11 +1,8 @@
 import formbody from '@fastify/formbody'
 import responseValidation from '@fastify/response-validation'
 import { error_response } from '@jackdbd/oauth2'
-import {
-  InvalidRequestError,
-  ServerError
-} from '@jackdbd/oauth2-error-responses'
 import { conformResult } from '@jackdbd/schema-validators'
+import { defErrorHandler } from '@repo/error-handlers'
 import { Type } from '@sinclair/typebox'
 import { Ajv, type Plugin as AjvPlugin } from 'ajv'
 import addFormats from 'ajv-formats'
@@ -179,51 +176,12 @@ const tokenEndpoint: FastifyPluginCallback<Options> = (
     })
   )
 
-  fastify.setErrorHandler((error, request, reply) => {
-    const code = error.statusCode
-
-    // Think about including these data error_description:
-    // - some JWT claims (e.g. me, scope)
-    // - jf2 (e.g. action, content, h, url)
-    // const claims = request.requestContext.get("access_token_claims");
-    // const jf2 = request.requestContext.get("jf2");
-    // console.log("=== claims ===", claims);
-    // console.log("=== jf2 ===", jf2);
-
-    if (code && code >= 400 && code < 500) {
-      request.log.warn(
-        `${logPrefix}client error catched by error handler: ${error.message}`
-      )
-    } else {
-      request.log.error(
-        `${logPrefix}server error catched by error handler: ${error.message}`
-      )
-    }
-
-    if (error.validation && error.validationContext) {
-      if (code && code >= 400 && code < 500) {
-        const messages = error.validation.map((ve) => {
-          return `${error.validationContext}${ve.instancePath} ${ve.message}`
-        })
-        const error_description = messages.join('; ')
-        const err = new InvalidRequestError({ error_description })
-        return reply
-          .code(err.statusCode)
-          .send(err.payload({ include_error_description }))
-      }
-    }
-
-    // If it's not a client error, is it always a generic Internal Server Error?
-    // Probably we can return a HTTP 503 Service Unavailable (maybe use
-    // @fastify/under-pressure).
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#server_error_responses
-
-    const error_description = error.message
-    const err = new ServerError({ error_description })
-    return reply
-      .code(err.statusCode)
-      .send(err.payload({ include_error_description }))
-  })
+  fastify.setErrorHandler(
+    defErrorHandler({
+      includeErrorDescription: include_error_description,
+      logPrefix
+    })
+  )
 
   done()
 }
