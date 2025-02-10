dbeae7377df9adbada746b0037f7f5500bce40b5
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-02-10 11:38:55 +0100

    feat(unwrap): change default onError/onUndefinedValue handlers from process.exit to throw

---
+++ .changeset/thin-points-breathe.md
@@ -0,0 +1,5 @@
+---
+"@jackdbd/unwrap": major
+---
+
+Change `onError` / `onUndefinedValue` handlers from `process.exit(1)` to `throw`.
--- packages/unwrap/test/index.js
+++ packages/unwrap/test/index.js
@@ -1,5 +1,55 @@
 import { describe, it } from 'node:test'
+import { unwrap, unwrapP, defUnwrap } from '../lib/index.js'
 
 describe('unwrap', () => {
-  it.todo('add some tests')
+  it('throws an error that has the expected error message when value is undefined', (t) => {
+    const message = 'expected error message'
+
+    t.assert.throws(
+      () => {
+        unwrap({ error: new Error(message), value: undefined })
+      },
+      { message }
+    )
+  })
+
+  it('unwraps the value from the result object when there is no error', (t) => {
+    const expected = 123
+
+    t.assert.strictEqual(
+      unwrap({ error: undefined, value: expected }),
+      expected
+    )
+  })
+})
+
+describe('unwrapP', () => {
+  it('throws an error that has the expected error message when value is undefined', (t) => {
+    const message = 'expected error message'
+
+    t.assert.rejects(
+      unwrapP({
+        error: new Error(message),
+        value: undefined
+      }),
+      { message }
+    )
+  })
+
+  it('unwraps the value from the result object when there is no error', async (t) => {
+    const expected = 123
+
+    const actual = await unwrapP({ error: undefined, value: expected })
+    t.assert.strictEqual(actual, expected)
+  })
+})
+
+describe('defUnwrap', () => {
+  it('allows to pass a custom onError handler', (t) => {
+    const value_on_error = 'default value'
+    const unwrap = defUnwrap({ onError: (_error) => value_on_error })
+
+    const actual = unwrap({ error: new Error('foo'), value: undefined })
+    t.assert.equal(actual, value_on_error)
+  })
 })
