022ffc46aa3acf15fc87847a26439f2f3be82554
Giacomo Debidda <giacomo@giacomodebidda.com>
2025-02-16 01:33:48 +0100

    add npm script test:coverage in all packages

--- packages/oauth2-error-responses/test/index.js
+++ packages/oauth2-error-responses/test/index.js
@@ -1,41 +1,40 @@
-import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import { InvalidRequestError, ServerError } from '../lib/index.js'
 
 describe('InvalidRequestError', () => {
-  it('has status code 400', () => {
+  it('has status code 400', (t) => {
     const error_description = 'Some client error'
     const err = new InvalidRequestError({ error_description })
 
-    assert.equal(err.statusCode, 400)
+    t.assert.equal(err.statusCode, 400)
   })
 
-  it('has a payload with `error` but no `error_description` by default', () => {
+  it('has a payload with `error` but no `error_description` by default', (t) => {
     const error_description = 'Some client error'
     const err = new InvalidRequestError({ error_description })
 
     const payload = err.payload()
 
-    assert.ok(payload.error)
-    assert.equal(payload.error_description, undefined)
+    t.assert.ok(payload.error)
+    t.assert.equal(payload.error_description, undefined)
   })
 
-  it('has a payload with `error` and also `error_description` if option include_error_description is true', () => {
+  it('has a payload with `error` and also `error_description` if option include_error_description is true', (t) => {
     const error_description = 'Some client error'
     const err = new InvalidRequestError({ error_description })
 
     const payload = err.payload({ include_error_description: true })
 
-    assert.ok(payload.error)
-    assert.equal(payload.error_description, error_description)
+    t.assert.ok(payload.error)
+    t.assert.equal(payload.error_description, error_description)
   })
 })
 
 describe('ServerError', () => {
-  it('has status code 500', () => {
+  it('has status code 500', (t) => {
     const error_description = 'Some client error'
     const err = new ServerError({ error_description })
 
-    assert.equal(err.statusCode, 500)
+    t.assert.equal(err.statusCode, 500)
   })
 })
